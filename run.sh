fb1d5eb2e4f90ac62d7a37331d061b868476c999
Artyom <rib.artem@gmail.com>
2021-04-09 21:53:24 +0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+#echo "Directory: $DIR"
 name="DBot"
 if ! screen -list | grep -q $name; then
     screen -dmS $name
 fi
-screen -S $name -X stuff 'npm run start
-'
+screen -S $name -p 0 -X eval 'stuff "npm run start"\015'
